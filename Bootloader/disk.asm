fc4b6e1af6bb81d19d24401a46a88133133b1eac
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-14 13:11:01 -0300

    Update disk.asm

--- Bootloader/disk.asm
+++ Bootloader/disk.asm
@@ -1,1 +1,21 @@
+[BITS    16]
 
+;AL = Sectors to read
+;CH = Cylinder
+;DH = Head
+;CL = Sector
+;ES:BX = Point to buffer
+ReadSectors:
+        push    ax
+        push    cx
+        push    dx
+
+        ;Read Sectors function
+        mov     ah, 0x02
+        int     0x13
+
+        pop     dx
+        pop     cx
+        pop     ax
+
+        ret
