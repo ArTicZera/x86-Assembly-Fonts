12ce5d4e1b93d146fc1dd0b5c28fe40708c4a21f
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-14 13:21:42 -0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+clear
+
+nasm -fbin Bootloader/boot.asm -o Binaries/boot.bin
+nasm -fbin Kernel/kernel.asm -o Binaries/kernel.bin
+
+if [ $? -eq 0 ]; then
+    echo -e "\n\e[36;40mCompiled successfully!\e[0m\n"
+else
+    echo -e "\n\e[31;40mError compiling!\e[0m\n"
+fi
+
+cat Binaries/boot.bin Binaries/kernel.bin > test.img
