af7b44ebdb0bb34e6dbbd718d6434cf33101ec6c
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-20 13:45:11 -0300

    Update kernel.asm

--- Kernel/kernel.asm
+++ Kernel/kernel.asm
@@ -2,17 +2,92 @@
 [ORG 0x7E00]
 
 KernelMain:
-        mov     si, string  ;SI = String
-        mov     al, 0x0F    ;AL = Color
-        mov     di, 0x00    ;DI = X Cursor
-        mov     bp, 0x00    ;BP = Y Cursor
-        call    PrintString
+        xor     bx, bx
+
+        ;jmp     CharTest
+        jmp     StringTest
 
         jmp     $
 
+StringTest:
+        mov     al, 0x00
+
+.StringLoop:
+        mov     si, testestr0
+        call    PrintString
+
+        inc     al
+
+        mov     si, testestr1
+        call    PrintString
+
+        inc     al
+
+        mov     si, testestr2
+        call    PrintString
+        
+        inc     al
+
+        mov     si, testestr3
+        call    PrintString
+
+        inc     al
+
+        mov     si, testestr4
+        call    PrintString
+
+        inc     al
+
+        mov     si, testestr5
+        call    PrintString
+
+        inc     al
+
+        mov     si, testestr6
+        call    PrintString
+
+        inc     al
+
+        mov     si, testestr7
+        call    PrintString
+
+        inc     al
+
+        inc     bx
+
+        cmp     bx, 25
+        jb      .StringLoop
+
+        jmp     $
+
+CharTest:
+        mov     bx, 0x00
+        mov     al, 0x01
+        mov     si, charA
+
+        .Charloop:
+                call    DrawChar
+                
+                inc     al
+                inc     bx
+
+                cmp     bx, 160
+                jnae    .Charloop
+
+                jmp     $
+
+%include "Bootloader/print.asm"
 %include "Font/fontbmp.asm"
 %include "Font/font.asm"
 
-string: db "A QUICK BROWN FOX JUMP OVER THE LAZY DOG! a quick brown fox jumps over the lazy dog! 0123456789 @#$%^&*-+(){}/\|.,':><=_", 0x00
+testestr0: db "Test0", 0x00, 0x00
+testestr1: db "Test1", 0x00, 0x00
+testestr2: db "Test2", 0x00, 0x00
+testestr3: db "Test3", 0x00, 0x00
+testestr4: db "Test4", 0x00, 0x00
+testestr5: db "Test5", 0x00, 0x00
+testestr6: db "Test6", 0x00, 0x00
+testestr7: db "Test7", 0x00, 0x00
+quickfox:  db "A Quick Brown Fox Jumps Over The Lazy Dog!", 0x00, 0x00
 
 times (510 * 8) - ($ - $$) db 0x00
